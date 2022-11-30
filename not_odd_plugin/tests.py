ae062162b525b352e576c9bc2f9575892ed511a9
Mikhail Poliakov <mikhail.polyakov@tessian.com>
2022-11-30 12:35:36 +0000

    Added tests.

---
+++ not_odd_plugin/tests.py
@@ -0,0 +1,33 @@
+import astroid
+import not_odd_plugin
+import pylint.testutils
+
+
+class TestNotOddChecker(pylint.testutils.CheckerTestCase):
+    CHECKER_CLASS = not_odd_plugin.NotOddChecker
+
+    def test_good(self):
+        node = astroid.extract_node(
+            """
+        x = 2 #@
+        """
+        )
+
+        with self.assertNoMessages():
+            self.checker.visit_assign(node)
+
+    def test_bad(self):
+        node = astroid.extract_node(
+            """
+        x = 1 #@
+        """
+        )
+
+        with self.assertAddsMessages(
+            pylint.testutils.MessageTest(
+                msg_id="not-odd",
+                node=node,
+            ),
+            ignore_position=True,
+        ):
+            self.checker.visit_assign(node)
