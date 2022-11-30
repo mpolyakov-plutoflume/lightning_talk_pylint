f41b483dc79b95fd17178672108e1422cce50a68
Mikhail Poliakov <mikhail.polyakov@tessian.com>
2022-11-30 12:16:06 +0000

    Scaffolding.

---
+++ not_odd_plugin/__init__.py
@@ -0,0 +1,26 @@
+import astroid
+from astroid import nodes
+from typing import TYPE_CHECKING
+
+from pylint.checkers import BaseChecker
+
+if TYPE_CHECKING:
+    from pylint.lint import PyLinter
+
+
+class NotOddChecker(BaseChecker):
+
+    name = "not-odd"
+    msgs = {
+        "W0001": (
+            "Odd numbers are not allowed",
+            "not-odd",
+            "All numbers should be even.",
+        ),
+    }
+
+def register(linter: "PyLinter") -> None:
+    """This required method auto registers the checker during initialization.
+    :param linter: The linter to register the checker to.
+    """
+    linter.register_checker(NotOddChecker(linter))
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="not_odd_plugin",
+    packages=find_packages(),
+)
